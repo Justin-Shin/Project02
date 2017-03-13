b3eb38cd9d5dda6c3210abfe2e40028a6afede60
superbuggy <jpqreichard@gmail.com>
2017-03-13 12:25:56 -0400

    add 2nd round of comments

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -58,25 +58,31 @@ class RecipesController < ApplicationController
     redirect_to root_path
   end
 
+  # Would just add a favorites_controller with index, create, & destroy methods
+
+  # should probably be index in favorites_controller
   def favorites
+    # @executable_faves maybe. moar semanticity!!
     @recipes = current_user.recipes.find_all do |recipe|
        recipe.foods.all? do |food|
          current_user.foods.include? food
       end
     end
+
     @recipes_no = current_user.recipes.find_all  do |recipe|
       @recipes.exclude? recipe
     end
-    puts @recipes, @recipes_no
   end
 
-  # Would just add a favorites_controller with just create & destory methods
+
+  # create in favorites_controller
   def add_favorite
     @recipe = Recipe.find(params[:id])
     @recipe.favorites.create(user: current_user)
     redirect_to :back
   end
 
+  # destroy in favorites_controller
   def remove_favorite
     Favorite.find_by(user: current_user, recipe_id: params[:id]).destroy
     redirect_to :back
@@ -84,8 +90,8 @@ class RecipesController < ApplicationController
 
 
   private
-    def recipe_params
-      params.require(:recipe).permit(:img_url,:name, :instructions, :servings)
-    end
+  def recipe_params
+    params.require(:recipe).permit(:img_url,:name, :instructions, :servings)
+  end
 
 end
